19818cb2bd1f610896f1343ac86c9ba9521180f0
satryawiguna <satrya@freshcms.net>
2023-03-30 16:52:59 +0800

    TDL-0012: Add default user role

---
+++ ToDo/Datas/Configuration/RoleConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ToDo.Datas.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Name = "administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                },
+                new IdentityRole
+                {
+                    Name = "guest",
+                    NormalizedName = "GUEST"
+                }
+            );
+        }
+    }
+}
+
---
+++ ToDo/Datas/Configuration/TodoTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ToDo.Datas.Configuration
+{
+    public class TodoTypeConfiguration : IEntityTypeConfiguration<TodoType>
+    {
+        public void Configure(EntityTypeBuilder<TodoType> builder)
+        {
+            builder.HasData(
+                new TodoType
+                {
+                    Id = 1,
+                    Title = "School activity"
+                },
+                new TodoType
+                {
+                    Id = 2,
+                    Title = "Work activity"
+                },
+                new TodoType
+                {
+                    Id = 3,
+                    Title = "Home activity"
+                }
+            );
+        }
+    }
+}
+
