01277a47ddd59081de6d0ba9c6cc26001fecad9e
satryawiguna <satrya@freshcms.net>
2023-03-31 07:13:19 +0800

    TDL-0012: Add default user role

--- ToDo/Configurations/MapperConfig.cs
+++ ToDo/Configurations/MapperConfig.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using ToDo.Datas;
 using ToDo.Models.Todo;
 using ToDo.Models.TodoType;
+using ToDo.Models.User;
 
 namespace ToDo.Configurations
 {
@@ -19,6 +20,8 @@ namespace ToDo.Configurations
             CreateMap<Todo, UpdateTodoDto>().ReverseMap();
             CreateMap<Todo, TodoDto>().ReverseMap();
             CreateMap<Todo, TodoWithTodoTypeDto>().ReverseMap();
+
+            CreateMap<User, RegisterDto>().ReverseMap();
         }
 	}
 }
---
+++ ToDo/Controllers/MembershipController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ToDo.Models.User;
+using ToDo.Repository.Contract;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDo.Controllers
+{
+    [Route("api/[controller]")]
+    public class MembershipController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+
+        public MembershipController(IAuthRepository authRepository)
+        {
+            this._authRepository = authRepository;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var errors = await _authRepository.Register(registerDto);
+
+            if (errors.Any())
+            {
+                foreach (var error in errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return Ok(registerDto);
+        }
+    }
+}
+
